fd1bbef8c4b8bc6edd6b6c8e52a077fc9fb11cbd
hamza oubouzid <hamza.oubouzid@neo-techit.com>
2024-11-07 22:48:29 +0100

    change  project  .net 7  to  .net 8

--- CustomInputMaui/ViewModels/AuthentificationViewModel.cs
+++ CustomInputMaui/ViewModels/AuthentificationViewModel.cs
@@ -3,10 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Channels;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace CustomInputMaui.ViewModels
